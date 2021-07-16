6c08f06eae2078158ead9a14f490aff35a7bf8ad
imrishuroy <rishukumar.prince@gmail.com>
2021-07-16 12:14:29 +0530

    Relese to ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 main() {
   runApp(MyApp());
@@ -16,8 +19,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? _imageFile;
+  File? _image;
+
+  _getImage() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _imageFile = image;
+        _image = File(image.path);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,18 +49,43 @@ class HomeScreen extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          // if (_image != null)
+          //   Center(
+          //     child: Container(
+          //       height: 250.0,
+
+          //       child: Image.file(_image!),
+          //     ),
+          //   ),
           Center(
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade300,
-                  borderRadius: BorderRadius.circular(10)),
-              height: 250.0,
-              width: 250.0,
-              child: Center(
-                child: Icon(
-                  Icons.image,
-                  color: Colors.green,
-                ),
+            child: InkWell(
+              onTap: () {
+                _getImage();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade300,
+                    borderRadius: BorderRadius.circular(10)),
+                height: 250.0,
+                width: 250.0,
+
+                /// child: Center(
+                //   child: Icon(
+                //     Icons.image,
+                //     color: Colors.green,
+                //   ),
+                // )),
+                child: _image == null
+                    ? Center(
+                        child: Icon(
+                          Icons.image,
+                          color: Colors.green,
+                        ),
+                      )
+                    : Image.file(
+                        _image!,
+                        fit: BoxFit.cover,
+                      ),
               ),
             ),
           ),
@@ -49,6 +94,7 @@ class HomeScreen extends StatelessWidget {
             onPressed: () {},
             child: Text('Resize Image'),
           ),
+          // Image.file(File(_imageFile!.path))
         ],
       ),
     );
